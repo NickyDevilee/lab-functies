73485adc1ce4baefeaebfb6ce549df44fefa7cdc
NickyDevilee <99047338@mydavinci.nl>
2018-10-16 09:46:55 +0200

    -

--- opdracht4.js
+++ opdracht4.js
@@ -3,9 +3,13 @@ Bol4 applicatieontwikkelaar
 Week 8 - JavaScript
 opdracht: Lab - functies*/
 
-function tafels(argument) {
-	var aantal = parseInt(prompt('Hoeveel tafels wil je berekenen?'));
-		for (var i = 0; i < Things.length; i++) {
-			Things[i]
+var aantal = parseInt(prompt('Hoeveel tafels wil je berekenen?'));
+
+function tafels(getal) {
+	for (var i = 0; i <= getal ; i++) {
+		for (var j = 1; j <= 10; j++) {
 		}
+	}
 }
+
+tafels(1);
